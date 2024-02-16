61add0613efd4a214df1216a5ee951eeb23e0507
Vrajesh Techtic <vrajesh@techtic.agency>
2024-02-16 17:44:41 +0530

    Changed Index file to add SpeedInsights

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
